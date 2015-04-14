adfa9de5d6d2902ee37d1084ffe5c2fd4d576b62
Benjamin Falk <bn_falk@yahoo.com>
2015-04-14 08:05:03 -0500

    Adding a search to food, does it by name

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,6 @@
 class Food < ActiveRecord::Base
   has_many :meal_items
   has_many :meals, through: :meal_items
+
+  scope :search, ->(term) { where('name like ?', "%#{term}%") }
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 RSpec.describe Food, type: :model do
   it { should have_many(:meal_items) }
   it { should have_many(:meals) }
+
+  context 'With a food named "roflmeat"' do
+    let!(:roflmeat) { FactoryGirl.create :food, name: 'roflmeat' }
+    let!(:hamburger) { FactoryGirl.create :food, name: 'hamburger' }
+    subject { Food.search('rofl') }
+    it { is_expected.to include(roflmeat) }
+    it { is_expected.to_not include(hamburger) }
+  end
 end
