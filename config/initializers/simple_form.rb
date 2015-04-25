4c51af654305fc745b5bdad9984aa36673c29319
Benjamin Falk <benjamin.falk@yahoo.com>
2015-04-25 11:39:27 -0500

    Cleaning up some form views and addign logout button

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -5,9 +5,10 @@ SimpleForm.setup do |config|
   # wrapper, change the order or even add your own to the
   # stack. The options given below are used to wrap the
   # whole input.
-  config.wrappers :default, class: :input,
-                            hint_class: :field_with_hint,
-                            error_class: :field_with_errors do |b|
+  config.wrappers :default,
+                  class: 'input form-group',
+                  hint_class: :field_with_hint,
+                  error_class: :field_with_errors do |b|
     ## Extensions enabled by default
     # Any of these extensions can be disabled for a
     # given input by passing: `f.input EXTENSION_NAME => false`.
@@ -62,7 +63,7 @@ SimpleForm.setup do |config|
   config.boolean_style = :nested
 
   # Default class for buttons
-  config.button_class = 'btn'
+  config.button_class = 'btn btn-default'
 
   # Method used to tidy up errors. Specify any Rails Array method.
   # :first lists the first message for each field.
@@ -153,7 +154,7 @@ SimpleForm.setup do |config|
   # config.cache_discovery = !Rails.env.development?
 
   # Default class for inputs
-  # config.input_class = nil
+  config.input_class = 'form-control'
 
   # Define the default class of the input wrapper of the boolean input.
   config.boolean_label_class = 'checkbox'
